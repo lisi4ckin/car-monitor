9399a659b14ead8b3b6b0ef32ab01df940fe53f9
Alexander Lisicin <lisitsin-shura@mail.ru>
2021-08-13 22:24:25 +0300

    Added functions to parse objects

--- main.py
+++ main.py
@@ -28,7 +28,34 @@ def collect_all_links(soup):
     return finish_links
 
 
+def str_to_int(str):
+    str = ''.join([c for c in str if c.isdigit()])
+    return int(str)
+
+
+def parse_info_from_one_car(url, cars):
+    soup = get_soup_object(url)
+    mark = soup.find('h1', class_='CardHead__title').text
+    price = str_to_int(soup.find("span", {"class": "OfferPriceCaption__price"}).text)
+    year_of_release = soup.find("a", {"class": "Link Link_color_black"}).text
+    mileage = str_to_int(soup.find("li", {"class": "CardInfoRow CardInfoRow_kmAge"}).text)
+    body_type = soup.find("li", {"class": "CardInfoRow CardInfoRow_bodytype"}).text[:5]
+    cars.append({
+        "mark": mark,
+        "link": url,
+        "info": {
+            "price": price,
+            "year_of_release": year_of_release,
+            "mileage": mileage,
+            "body type": body_type
+        }
+    })
+
+
 if __name__ == '__main__':
     soup = get_soup_object(BASE_URL.format(1))
-    links = collect_all_links(soup)
-    print(collect_all_links(soup))
+    links = collect_all_links(soup)  # this function collect all links from page http:://auto.ru
+    cars = []
+    for link in links:
+        parse_info_from_one_car(link, cars)
+    print(cars)
